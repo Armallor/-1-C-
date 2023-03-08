97a12abf24e3f6096be731b13c0fb705ba68aafd
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-03-08 17:25:04 +0300

    Решение четвертой задачи

---
+++ Quest_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.WriteLine("Введите первое целое число");
+
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе целое число");
+
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите третье целое число");
+
+int C = int.Parse(Console.ReadLine()!);
+
+int MAX = A;
+
+if (B > MAX) MAX = B;
+
+if (C > MAX) MAX = C;
+
+Console.WriteLine($"Максимальное число равно {MAX}");
