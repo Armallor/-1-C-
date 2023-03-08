dc2a39fe1ae13324d5a66954a7cee2e57dc1db7a
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-03-08 17:22:56 +0300

    Решение второй задачи

---
+++ Quest_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее;
+
+Console.WriteLine("Введите первое целое число");
+
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе целое число");
+
+int B = int.Parse(Console.ReadLine()!);
+
+if (A > B) Console.WriteLine($"Число {A} больше числа {B}");
+
+else Console.WriteLine($"Число {B} больше числа {A}");
