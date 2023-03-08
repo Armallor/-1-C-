d953d26ba102e0d47808525ff7c32a9d3d154189
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-03-08 17:49:11 +0300

    Решение восьмой задачи

---
+++ Quest_8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает
+// все чётные числа от 1 до N
+
+Console.WriteLine("Введите число");
+
+int A = int.Parse(Console.ReadLine()!);
+
+while (A > 1){
+if (A%2 == 0) Console.WriteLine(A);
+A = A - 1;
+}
